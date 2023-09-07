1d5014b65127baf29a1583c3f96fb79c4a40d8e9
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-09-08 00:04:31 +0100

    Add error spec and refund spec

--- spec/govpay_integration_spec.rb
+++ spec/govpay_integration_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe GovpayIntegration do
   it "has a version number" do
     expect(GovpayIntegration::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end

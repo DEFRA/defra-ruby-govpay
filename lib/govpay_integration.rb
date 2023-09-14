6aee801dff3b0cd48bfd472946e8d1599c374279
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-09-14 15:16:31 +0100

    Remove code for unused objects

--- lib/govpay_integration.rb
+++ lib/govpay_integration.rb
@@ -3,10 +3,6 @@
 require "json"
 require_relative "govpay_integration/version"
 require_relative "govpay_integration/configuration"
-require_relative "govpay_integration/object"
-require_relative "govpay_integration/payment"
-require_relative "govpay_integration/refund"
-require_relative "govpay_integration/error"
 require_relative "govpay_integration/api"
 
 # The GovpayIntegration module facilitates integration with Govpay services.
